237f761f63cec00cf50ac43fcd1b2c07dc049313
Tiramisu Silva <ss.inam124@gmail.com>
2023-04-10 13:29:14 -0600

    Update BarNav.jsx

--- src/componentes/BarNav.jsx
+++ src/componentes/BarNav.jsx
@@ -7,6 +7,93 @@ import { theme } from '../theme';
 const StyledNavBar = styled.nav`
     background-color: ${theme.palette.primary.main};
     color: ${theme.palette.primary.contrastText};
+    display: flex;
+    justify-content:space-between;
+    align-items: center;
+    width: 95vw;
+    height: 15vh;
+    font-size: 1.2rem;
+    padding: 10px 30px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    position: fixed;
+    top : 10px;
+    left: 50%;
+    transform: translate(-50%);
+
+    .logo-uam{
+        height: 100%;
+    }
+
+    .barra-lista-enlaces{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 5px;
+        height: 70%;
+        width: 70%;
+    }
+
+    .barra-lista-enlaces li{
+        list-style: none;
+        
+    }
+
+    .icono-barra{
+        visibility: hidden;
+    }
+
+    @media screen and (max-height: 550px) {
+
+        width: 100vw;
+        height: 10vh;
+        padding: 5px;
+        font-size: 0.8rem;
+        
+    }
+
+    @media screen and (max-width: 768px) {
+        height: 10vh;
+
+        .barra-lista-enlaces{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: flex-start;
+            position: fixed;
+            top: 60px;
+            right: -500px;
+            width: 300px;
+            height: 100vh;
+            background-color: blanchedalmond;
+            color: black;
+            box-shadow: 0 40px 60px rgba(0, 0, 0, 0.1);
+            padding: 40px 0 0 10px;
+            transition: 0.3s ease-in-out;
+        }
+
+        .barra-lista-enlaces.active{
+            right: 0;
+        }
+
+        .barra-lista-enlaces li{
+            flex: 0;
+            margin-bottom: 25px;
+            margin-left: 25px;
+            list-style: initial;
+        }
+
+        .icono-barra{
+            display: block;
+            visibility: visible;
+        }
+
+        .icono-barra button{
+            display: block;
+            visibility: visible;
+        }
+
+    }
     
 `;
 
@@ -27,7 +114,7 @@ export function BarNav () {
 
             <img className='logo-uam' src={UAM} />
 
-            <ul className='barra-lista-enlaces'>
+            <ul className={`${showMenu ? " barra-lista-enlaces active" : "barra-lista-enlaces "}`}>
                 <li className='barra-enlace'>
                     <h2>holiii</h2>
                 </li>
